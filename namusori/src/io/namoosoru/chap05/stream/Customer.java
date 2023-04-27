b67a7ae84eced2a1d092fac80ab285cc161f5e7c
JangDongyul <purplebeige12@gmail.com>
2023-04-27 15:09:03 +0900

    namoosori_Stream1

---
+++ namusori/src/io/namoosoru/chap05/stream/Customer.java
@@ -0,0 +1,51 @@
+package io.namoosoru.chap05.stream;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Customer {
+    private String name;
+    private int age;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return age == customer.age && Objects.equals(name, customer.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
---
+++ namusori/src/io/namoosoru/chap05/stream/StreamAssist.java
@@ -0,0 +1,24 @@
+package io.namoosoru.chap05.stream;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamAssist {
+    public static void main(String[] args) {
+        List<Customer> customers = new ArrayList<>();
+
+        customers.add(new Customer("Song",45));
+        customers.add(new Customer("Kim",35));
+        customers.add(new Customer("Park",21));
+        customers.add(new Customer("Lee",67));
+        customers.add(new Customer("Choi",19));
+
+        customers.stream().filter(customer -> customer.getAge()>30).sorted(Comparator.comparing(Customer::getAge).reversed()).map(Customer::getName).forEach(System.out::println);
+
+        List<String> customers2 = customers.stream().filter(customer->customer.getAge()>30).map(Customer::getName).collect(Collectors.toList());
+        String a = customers2.get(2);
+        customers2.add("ss");
+    }
+}
